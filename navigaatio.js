b3fb57412c86df41d70a757471c8a5c210ce18c0
teme <teemu.taponen22@gmail.com>
2021-12-02 10:48:36 +0200

    muutokset

--- navigaatio.js
+++
@@ -1,9 +0,0 @@
-//Navigaatio
-function navFunction() { //luodaan function ylÃ¤navipalkille
-    let navnav = document.getElementById("ylaNavi"); //luodaan muuttuja ja haetaan sille id:llÃ¤ oikea elementti
-    if (navnav.className === "topnav") { // Jos nÃ¤ytÃ¶n koko pienenee niin tÃ¤llÃ¤ muutetaan millÃ¤ css class jatketaan
-    navnav.className += " responsive";
-} else {
-    navnav.className = "topnav";
-}
-}
