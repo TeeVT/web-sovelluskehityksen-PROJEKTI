41fffa99483e7dd2e86b72fa36a80b3e660bd835
teme <teemu.taponen22@gmail.com>
2021-12-15 14:36:16 +0200

    virtual

--- app.js
+++ app.js
@@ -9,8 +9,14 @@ const passport = require('./utils/pass');
 const { httpError } = require('./utils/errors');
 
 const app = express();
-const port = 3000;
 app.use(cors());
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+if (process.env.NODE_ENV === 'production') {
+  require('./utils/production')(app, process.env.PORT, process.env.HTTPS_PORT);
+} else {
+  require('./utils/localhost')(app, process.env.PORT);
+}
 
 app.use(express.json()); // for parsing application/json
 app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
@@ -34,5 +40,3 @@ app.use((err, req, res, next) => {
     message: err.message || 'internal server error',
   });
 });
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
